4616253494b1309136235fc4d684320ee60763f1
PraptiH <praptiantara1892@gmail.com>
2026-04-27 23:29:52 +0600

    Add route-based dynamic page loader with pushState navigation

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const navbar = document.getElementById('navbar')
-window.addEventListener('scroll',()=>{
-    console.log('screenY')
-    if(window.scrollY>10){
+
+const routes = {
+    '/appstore.html': 'appstore.html',
+    '/roommateFinder.html': 'roommateFinder.html'
+}
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
         navbar.classList.add('navColor')
     }
-    else{
+    else {
         navbar.classList.remove('navColor')
     }
 })
+
+function loadPage(path) {
+    const file = routes[path];
+    if (!file) {
+        document.getElementById('content').innerHTML = `<h1>Page Not Found</h1>`
+        return
+    }
+
+    fetch(`/${file}`)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html
+        })
+    window.history.pushState({}, "", path)
+}
